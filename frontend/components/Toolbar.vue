8a541c077409d038c810993d26f94835a249d9c3
nitpum <nitpum@outlook.com>
2019-04-09 00:17:24 +0700

    Add component/Toolbar

---
+++ frontend/components/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar clipped-left="true" fixed app>
+    <v-toolbar-side-icon @click="drawer = !drawer" />
+    <v-toolbar-title v-text="title" />
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  props: {
+    drawer: {
+      type: Boolean,
+      default: true
+    },
+    title: {
+      type: String,
+      default: 'Yee Netbank'
+    }
+  }
+}
+</script>

9fcbfa87e786ebbceaa0be6efd3512de899d42d8
nitpum <nitpum@outlook.com>
2019-04-08 23:26:00 +0700

    Add centered layout

---
+++ frontend/layouts/centered.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <nuxt />
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>

f63ffab3fb08dea2b24fe1b3895d6a59deef7b3c
topty <topty@live.com>
2019-04-09 18:32:48 +0700

    homepage layout

---
+++ frontend/components/default/Drawer.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-navigation-drawer v-model="model" overflow right app>
+    <v-list>
+      <v-list-tile v-for="(item, i) in menu" :key="'menu-' + i" :to="item.to">
+        <v-list-tile-content>
+          <v-list-tile-title>
+            {{ item.name }}
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import { menu } from './menu.json'
+
+export default {
+  props: {
+    drawer: Boolean
+  },
+  data: () => ({
+    menu
+  }),
+  computed: {
+    model: {
+      get() {
+        return this.drawer
+      },
+      set(val) {
+        this.$emit('update:drawer', val)
+      }
+    }
+  }
+}
+</script>

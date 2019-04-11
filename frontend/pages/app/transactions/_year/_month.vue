be0143a8db8ed8f6d652914d42668b1fe4d74a92
topty <topty@live.com>
2019-04-11 12:29:35 +0700

    increase transactions card size

--- frontend/pages/app/transactions/_year/_month.vue
+++ frontend/pages/app/transactions/_year/_month.vue
@@ -3,12 +3,12 @@
     <month-tab />
     <v-container>
       <v-layout justify-center mb-5>
-        <v-flex xs12 sm7 md5 lg4 xl3>
+        <v-flex xs12 sm7 md5 lg5 xl4>
           <Balance />
         </v-flex>
       </v-layout>
       <v-layout justify-center>
-        <v-flex xs12 sm7 md5 lg4 xl3>
+        <v-flex xs12 sm7 md5 lg5 xl4>
           <Date />
           <Date />
         </v-flex>

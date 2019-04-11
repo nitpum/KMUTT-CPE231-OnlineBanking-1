766a931eb651ed47b69164f4cf7830bb1868ab1d
topty <topty@live.com>
2019-04-11 14:41:31 +0700

    + transactions child route

--- frontend/pages/app/transactions/_year/_month.vue
+++ frontend/pages/app/transactions/_year/_month.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <month-tab />
     <v-container>
       <v-layout justify-center mb-5>
         <v-flex xs12 sm7 md5 lg5 xl4>
@@ -19,14 +18,12 @@
 
 <script>
 import Balance from '@/components/customer/transactions/Balance'
-import MonthTab from '@/components/customer/transactions/MonthTab'
 import Date from '@/components/customer/transactions/Date'
 
 export default {
   layout: 'customer',
   components: {
     Balance,
-    MonthTab,
     Date
   }
 }

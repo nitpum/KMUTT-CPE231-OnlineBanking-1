2e2ef43266b3960908d4faaa48605b5ef7559bd8
topty <topty@live.com>
2019-04-11 13:34:44 +0700

    bill payment card

--- frontend/components/customer/bills/favorite/index.vue
+++ frontend/components/customer/bills/favorite/index.vue
@@ -1,15 +1,24 @@
 <template>
   <v-card>
     <toolbar />
+    <v-container fluid grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 lg4 xl3>
+          <bill-card />
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-card>
 </template>
 
 <script>
 import Toolbar from './Toolbar'
+import BillCard from './BillCard'
 
 export default {
   components: {
-    Toolbar
+    Toolbar,
+    BillCard
   }
 }
 </script>

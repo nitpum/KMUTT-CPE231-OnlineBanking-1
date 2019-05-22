b316846eba011bc6ab0949618418e688803cbe99
nitpum <nitpum@outlook.com>
2019-05-22 18:09:02 +0700

    Drawer support child item

--- frontend/components/Drawer.vue
+++ frontend/components/Drawer.vue
@@ -9,14 +9,43 @@
   >
     <slot />
     <v-list>
-      <v-list-tile v-for="(item, i) in items" :key="i" :to="item.to">
-        <v-list-tile-action>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title v-text="item.title" />
-        </v-list-tile-content>
-      </v-list-tile>
+      <template v-for="(item, i) in items">
+        <template v-if="item.children">
+          <v-list-group :key="i" v-model="item.model" :prepend-icon="item.icon">
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.title }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile
+              v-for="(child, j) in item.children"
+              :key="j"
+              :to="child.to"
+            >
+              <v-list-tile-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
+        </template>
+        <template v-else>
+          <v-list-tile :key="i" :to="item.to">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.title" />
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </template>
     </v-list>
   </v-navigation-drawer>
 </template>

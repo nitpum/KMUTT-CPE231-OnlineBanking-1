8bd26239d66604e8b0cd95cbf68cd0f09908369e
yee2542 <yee2542gtl@gmail.com>
2019-05-24 00:54:30 +0700

    add custom authen middleware

--- backend/controllers/helpers/authen.js
+++ backend/controllers/helpers/authen.js
@@ -3,10 +3,17 @@
 //   return next()
 // }
 
-module.exports = () => {
+/**
+ * @param  {Object} options - setting for authen middleware
+ * @param  {String} options.permission - permission
+ * @param  {String} options.unauthorizedPath - redirect after fail login
+ * @param  {String} options.authorizedPath - redirect after success login
+ */
+module.exports = function (options) {
   return (req, res, next) => {
-      console.log('authen')
-    next()
+    if (!req.session) return res.redirect(options.unauthorizedPath)
+    const user = req.session.passport.user
+    if (user.permission === options.permission) return next()
+    else return res.redirect(options.unauthorizedPath)
   }
 }
-

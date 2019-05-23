71bc9187b6f68081ba34a753b4a99ae600a3994d
yee2542 <yee2542gtl@gmail.com>
2019-05-23 21:15:55 +0700

    add doc for password tools

--- backend/models/helpers/password-hash.js
+++ backend/models/helpers/password-hash.js
@@ -11,7 +11,11 @@ const hash = (password) => new Promise((resolve, reject) => {
     })
   })
 })
-
+/**
+ * @param  {String} password - password
+ * @param  {String} hash - hash
+ * @returns {Boolean} - true/false
+ */
 const compare = (password, hash) => new Promise((resolve, reject) => {
   bcrypt.compare(password, hash, (err, res) => {
     if (err) reject(err)

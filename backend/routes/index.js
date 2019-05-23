e448c5416f3e97a626e38f0210cfd92f3ff587ce
yee2542 <yee2542gtl@gmail.com>
2019-05-23 19:18:39 +0700

    add doc

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,9 @@
 const admin = require('../controllers/admin')
 
+/**
+ * router HTTP function top on express.js
+ * @param  {Object} app - only accept express router function, method
+ */
 module.exports = app => {
   // controllers
   app.use('/admin', admin)

10edf60b6a6b7b897715ac53083d3139953f0333
nitpum <nitpum@outlook.com>
2019-05-23 23:15:24 +0700

    Finish staff/cheque

---
+++ frontend/components/staff/cheque/CreateDialog.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-dialog v-model="model" lazy max-width="500">
+    <v-card>
+      <v-card-title primary-title>
+        <h2>Create Cheque</h2>
+      </v-card-title>
+      <v-card-text>
+        <text-label label="Type" placeholder="Type"></text-label>
+        <text-label label="Bank" placeholder="Yee Bank"></text-label>
+        <v-select label="Bank" placeholder="Bank" :items="banks"></v-select>
+        <v-text-field
+          label="Account ID"
+          placeholder="Account ID"
+        ></v-text-field>
+        <v-text-field
+          label="Amount"
+          placeholder="0.00 THB"
+          type="number"
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn flat @click="model = false">Close</v-btn>
+        <v-btn color="primary" flat>Submit</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import TextLabel from '@/components/core/TextLabel'
+
+export default {
+  components: {
+    TextLabel
+  },
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    banks: ['Yee bank']
+  }),
+  computed: {
+    model: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
+    }
+  }
+}
+</script>
--- frontend/pages/staff/cheque.vue
+++ frontend/pages/staff/cheque.vue
@@ -1,29 +1,48 @@
 <template>
   <div>
     <v-container>
-      <v-layout justify-center>
-        <v-flex>
-          <v-card>
-            <v-card-title primary-title></v-card-title>
-          </v-card>
+      <v-btn color="accent" fixed bottom fab right @click="createDialog = true">
+        <v-icon>add</v-icon>
+      </v-btn>
+      <v-text-field
+        label="Search"
+        placeholder="Search"
+        prepend-inner-icon="search"
+        solo
+      ></v-text-field>
+      <v-layout row wrap>
+        <v-flex v-for="(cheque, i) in cheques" :key="i" md4 sm12 xs12 mb-3 pr-3>
+          <card :show-pay-btn="true" @onPay="pay(cheque)"></card>
         </v-flex>
       </v-layout>
     </v-container>
-    <pay-dialog></pay-dialog>
+    <pay-dialog v-model="payDialog"></pay-dialog>
+    <create-dialog v-model="createDialog"></create-dialog>
   </div>
 </template>
 
 <script>
+import Card from '@/components/core/cheque/Card'
 import PayDialog from '@/components/staff/cheque/PayDialog'
+import CreateDialog from '@/components/staff/cheque/CreateDialog'
 
 export default {
   layout: 'staff',
   components: {
-    PayDialog
+    Card,
+    PayDialog,
+    CreateDialog
   },
   data: () => ({
     account: null,
-    dialog: false
-  })
+    payDialog: false,
+    createDialog: false,
+    cheques: [{}]
+  }),
+  methods: {
+    pay: function(cheque) {
+      this.payDialog = true
+    }
+  }
 }
 </script>

487424a8193729890c7b4e16f27ee4995358a63c
yee2542 <yee2542gtl@gmail.com>
2019-05-23 19:15:59 +0700

    register admin controller

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,9 @@
+const admin = require('../controllers/admin')
+
 module.exports = app => {
+  // controllers
+  app.use('/admin', admin)
+
   app.use('*', (req, res) => {
     res.send('online banking system')
   })

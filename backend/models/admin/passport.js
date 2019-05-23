79435e8c578ccea6cce34cdcce7a17b3c4f41887
yee2542 <yee2542gtl@gmail.com>
2019-05-23 22:07:55 +0700

    add passport models

---
+++ backend/models/admin/passport.js
@@ -0,0 +1,42 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+
+const PERMISSION = 'admin'
+
+const AdminSchema = require('./schema')
+const AdminModel = require('./index')
+
+module.exports = passport => {
+  // used to serialize the user for the session
+  passport.serializeUser((user, done) => {
+    done(null, user)
+  })
+
+  // next should save only permission & id
+  passport.deserializeUser((id, done) => {
+    AdminSchema.findById(id, (err, user) => {
+      done(err, user)
+    })
+  })
+
+  passport.use(
+    'admin',
+    new LocalStrategy(
+      {
+        usernameField: 'username',
+        passwordField: 'password'
+      },
+      (username, password, done) => {
+        console.log(`admin access from : ${username}`)
+
+        AdminModel.login(username, password).then(doc => {
+          if (doc) {
+            doc.permission = PERMISSION
+            return done(null, doc)
+          }
+          return done(null, false)
+        })
+      }
+    )
+  )
+}

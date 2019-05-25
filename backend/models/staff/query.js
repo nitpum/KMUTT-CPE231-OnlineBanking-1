709ef0d7388d308325dfc25ae176778937bd8f89
yee2542 <yee2542gtl@gmail.com>
2019-05-25 23:42:48 +0700

    add query model

---
+++ backend/models/staff/query.js
@@ -0,0 +1,38 @@
+const StaffSchema = require('./schema')
+
+const query = {
+  /**
+       * get all branch
+       * @param  {Number} limit=5000 - list first 5000 staff order by indexed
+       * @returns {Array} - mongodb object
+       */
+  all: (limit = 5000) => new Promise((resolve, reject) => {
+    StaffSchema.find({})
+      .limit(limit)
+      .then(doc => resolve(doc))
+      .catch(err => reject(err))
+  }),
+
+  /**
+     * @param  {String} s
+     * @param  {Number} limit=1000 list search 1000 branch order by indexed
+     */
+  search: (s, limit = 1000) => new Promise((resolve, reject) => {
+    StaffSchema.find({ $text: { $search: s } })
+      .limit(limit)
+      .then(doc => resolve(doc))
+      .catch(err => reject(err))
+  }),
+  /**
+     * @param  {String} id - mongodb object id
+     * @returns {Object} - mongodb object
+     */
+  id: (id) => new Promise((resolve, reject) => {
+    StaffSchema.findById(id)
+      .then(doc => resolve(doc))
+      .catch(err => reject(err))
+  })
+
+}
+
+module.exports = query

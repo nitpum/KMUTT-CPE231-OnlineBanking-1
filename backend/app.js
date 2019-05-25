27c56c471db0ec1c9dfc157009f3b91440dae3a8
yee2542 <yee2542gtl@gmail.com>
2019-05-25 17:36:10 +0700

    add pug engine

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.set('view engine', 'pug')
 
 app.use(express.static(path.join(__dirname, 'public')))
 

f5ab042e4a903e6382d3fba1020925ee40f73314
nitpum <nitpum@outlook.com>
2019-05-26 18:47:00 +0700

    Update cheque

--- frontend/components/core/cheque/Card.vue
+++ frontend/components/core/cheque/Card.vue
@@ -1,19 +1,16 @@
 <template>
   <v-card>
     <v-card-title primary-title class="mb-0 pb-0">
-      <h3 class="headline mb-0">{{ accountId }}</h3>
+      <h3 class="headline mb-0">{{ chequeId }}</h3>
     </v-card-title>
-    <v-card-text class="pt-0 pb-1 grey--text">
-      {{ holder }}
-    </v-card-text>
-    <v-card-text v-if="branch" class="pt-0 pb-1 grey--text">
-      Branch: {{ branch }}
+    <v-card-text v-if="accountId" class="pt-0 pb-1 grey--text">
+      Account ID: {{ accountId }}
     </v-card-text>
     <v-card-text v-if="bank" class="pt-0 pb-1 grey--text">
       {{ bank }}
     </v-card-text>
-    <v-card-text v-if="balance" class="pt-0 pb-1 grey--text">
-      Balance: {{ balance }}
+    <v-card-text v-if="amount" class="pt-0 pb-1 grey--text">
+      Amount: {{ format(amount) }}
     </v-card-text>
     <v-divider class="mt-4" />
     <v-card-actions>
@@ -24,13 +21,27 @@
       <v-btn v-if="showPayBtn" flat @click="$emit('onPay')">
         Pay
       </v-btn>
+      <v-btn
+        v-if="showMoreInfo"
+        color="primary"
+        flat
+        @click="$emit('onMoreInfo')"
+      >
+        More Info
+      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import numeral from 'numeral'
+
 export default {
   props: {
+    chequeId: {
+      type: String,
+      default: '000-0-000000-0'
+    },
     accountId: {
       type: String,
       default: '000-0-000000-0'
@@ -43,13 +54,9 @@ export default {
       type: String,
       default: ''
     },
-    balance: {
+    amount: {
       type: Number,
       default: 0
-    },
-    branch: {
-      type: String,
-      default: 'Bangmod'
     },
     showRevokeBtn: {
       type: Boolean,
@@ -58,6 +65,15 @@ export default {
     showPayBtn: {
       type: Boolean,
       default: false
+    },
+    showMoreInfo: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    format(value) {
+      return numeral(value).format('0,0.00')
     }
   }
 }

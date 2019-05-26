902be9217c5434426a39c5bea24e19fe5801a495
yee2542 <yee2542gtl@gmail.com>
2019-05-26 17:47:35 +0700

    return simple data analytic

--- backend/controllers/customer/analytic.js
+++ backend/controllers/customer/analytic.js
@@ -6,7 +6,17 @@ const CustomerModel = require('../../models/customer/')
 
 router.get('/', async (req, res) => {
   const n = await CustomerModel.analytic.count()
-  res.send(n)
+  const bal = await CustomerModel.analytic.balance.all()
+
+  res.send({
+    ...n, ...bal
+  })
+})
+
+router.get('/', (req, res) => {
+  CustomerModel.analytic.count()
+    .then(n => res.send(n))
+    .catch(err => res.send(String(err)))
 })
 
 module.exports = router

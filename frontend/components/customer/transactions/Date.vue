83976da473602bc13f5701f7022aaa7db2f49c34
topty <topty@live.com>
2019-05-26 15:14:10 +0700

    format number in transactions card

--- frontend/components/customer/transactions/Date.vue
+++ frontend/components/customer/transactions/Date.vue
@@ -12,7 +12,7 @@
         </v-list-tile-content>
         <v-list-tile-content>
           <v-list-tile-title class="text-xs-right">
-            {{ item.amount }}
+            {{ format(item.amount) }}
           </v-list-tile-title>
           <v-list-tile-sub-title class="text-xs-right">
             {{ item.desc }}
@@ -32,15 +32,27 @@ export default {
         type: 'Purchase',
         desc: 'Google Play Store',
         time: '07:36:25 PM',
-        amount: '-62.17'
+        amount: -62.17
       },
       {
         type: 'Deposit',
         desc: 'via ADM',
         time: '10:10:39 AM',
-        amount: '+500.00'
+        amount: +500.0
       }
     ]
-  })
+  }),
+  methods: {
+    format(value) {
+      return [
+        value < 0 ? '' : '+',
+        typeof value === 'number'
+          ? Number(value)
+              .toFixed(2)
+              .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+          : value
+      ].join('')
+    }
+  }
 }
 </script>

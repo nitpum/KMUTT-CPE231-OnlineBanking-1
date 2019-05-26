9a77338112f500a3f36268bb4f3b0a6850044356
yee2542 <yee2542gtl@gmail.com>
2019-05-26 22:20:01 +0700

    add simple find range of ages customer

--- backend/models/customer/analytic.js
+++ backend/models/customer/analytic.js
@@ -1,10 +1,9 @@
+const moment = require('moment')
 const CustomerModel = require('./schema')
 
 // helpers
 const mapObjectIdHelpers = require('../helpers/map-mongodb-objectId')
 const generateDateRangeHelpers = require('../helpers/generate-date-range')
-
-generateDateRangeHelpers(range).then(n => console.log(n))
 
 const analytic = {
   balance: {
@@ -66,10 +65,59 @@ const analytic = {
   }),
 
   // 0-15 16-30 31-45 46-60 61-75 76-90 91-105+
-  age: () => {
+  age: () => new Promise(async (resolve, reject) => {
     const range = [0, 15, 30, 45, 60, 75, 90, 105]
-    return null
-  }
+    const result = []
+    const allPromise = []
+    let rangeDate = await generateDateRangeHelpers(range)
+    rangeDate.forEach((d, i) => {
+      allPromise.push(
+        eachDatePromise(rangeDate, result, d, i)
+      )
+      // CustomerModel.aggregate([
+      //   { $match: {
+      //     birthDate: {
+      //       $lt: rangeDate[i],
+      //       $gte: rangeDate[i + 1]
+      //     }
+      //   } },
+      //   { $count: 'n' }
+      // ]).then(doc => {
+      //   result.push({
+      //     n: doc[0] ? doc[0].n : 0,
+      //     range: [`${moment().year() - moment(d).year()}`, `${moment().year() - moment(rangeDate[i + 1]).year()}`]
+      //   })
+      // })
+    })
+
+    // console.log(allPromise)
+    await Promise.all(allPromise)
+    // console.log(result)
+    resolve(result)
+
+    // setTimeout(() => resolve(result), 3000)
+  })
 }
+
+const eachDatePromise = (rangeDate, output, d, i) => new Promise((resolve, reject) => {
+  CustomerModel.aggregate([
+    { $match: {
+      birthDate: {
+        $lt: rangeDate[i],
+        $gte: rangeDate[i + 1]
+      }
+    } },
+    { $count: 'n' }
+  ]).then(doc => {
+    resolve(
+      output.push({
+        n: doc[0] ? doc[0].n : 0,
+        range: [`${moment().year() - moment(d).year()}`, `${moment().year() - moment(rangeDate[i + 1]).year()}`]
+      })
+    )
+  })
+})
+
+analytic.age().then(res => console.log(res))
 
 module.exports = analytic

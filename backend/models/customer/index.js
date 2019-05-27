82c67914a5edb312ba7f3f2c6702316cc2fbfc57
yee2542 <yee2542gtl@gmail.com>
2019-05-27 15:47:53 +0700

    add edit function

--- backend/models/customer/index.js
+++ backend/models/customer/index.js
@@ -75,6 +75,48 @@ const create = (data) => new Promise(async (resolve, reject) => {
 })
 
 /**
+  * edit customer user
+ * @param  {Object} data
+ * @param  {String} data.username - username
+ * @param  {String} data.password - password
+ * @param  {String} data.email - email
+ * @param  {String} data.name.firstName - firstname
+ * @param  {String} data.name.lastName - firstname
+ * @param  {String} data.zipcode - zipcode
+ * @param  {String} data.address - address
+ * @param  {Date} data.birthDate - birthdate
+ * @param  {String} data.gender - ['M', 'F', 'U']
+ * @param  {String} data.citizenId - citizenId
+ * @param  {String} data.position - position
+ * @param  {String} data.phone - phone
+ * @returns {Object} - updated mongodb document
+ */
+const edit = (id, data) => new Promise(async (resolve, reject) => {
+  const {
+    username, password, name, zipcode,
+    address, birthDate, gender, phone,
+    citizenId, email, balance
+  } = data
+  const { firstName, lastName } = name
+  const hash = await passwordHelpers.generate(password)
+  const doc = await CustomerSchema.findByIdAndUpdate(id, {
+    username: username,
+    password: hash,
+    'name.firstName': firstName,
+    'name.lastName': lastName,
+    email: email,
+    zipcode: zipcode,
+    address: address,
+    birthDate: birthDate,
+    phone: phone,
+    gender: gender,
+    citizenId: citizenId,
+    balance: balance
+  })
+  resolve(doc)
+})
+
+/**
  * @param  {String} username
  * @param  {String} password
  * @returns {Object}
@@ -91,6 +133,7 @@ const login = (username, password) => new Promise(async (resolve, reject) => {
 module.exports = {
   schema: CustomerSchema,
   create: create,
+  edit: edit,
   login: login,
   query: QueryModel,
   analytic: AnalyticModel

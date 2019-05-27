1be6976715e28d5c7c2623a0f6ea01f7309b4f49
yee2542 <yee2542gtl@gmail.com>
2019-05-28 00:31:53 +0700

    change to send status code

--- backend/controllers/helpers/authen.js
+++ backend/controllers/helpers/authen.js
@@ -1,21 +1,16 @@
-// module.exports = (req, res, next, permission, failPath) => {
-//   console.log(req.session)
-//   return next()
-// }
-
 /**
  * @param  {Object} options - setting for authen middleware
  * @param  {Array} options.permission - permission
  * @param  {String} options.unauthorizedPath - redirect after fail login
  * @param  {String} options.authorizedPath - redirect after success login
  */
-module.exports = function (options) {
+module.exports = (options) => {
   return (req, res, next) => {
-    if (!req.session) return res.redirect(options.unauthorizedPath)
-    if (!req.session.passport) return res.redirect(options.unauthorizedPath)
+    if (!req.session) return res.sendStatus(401)
+    if (!req.session.passport) return res.sendStatus(401)
     const user = req.session.passport.user
     const compare = options.permission.some(e => e === user.permission)
     if (compare) return next()
-    else return res.redirect(options.unauthorizedPath)
+    else return res.sendStatus(401)
   }
 }

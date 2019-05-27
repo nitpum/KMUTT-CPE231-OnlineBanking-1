77c5fbe98151dedb7a0c17ed66177677bd897d5f
yee2542 <yee2542gtl@gmail.com>
2019-05-27 19:28:08 +0700

    add query, edit, delete function

--- backend/controllers/admin/index.js
+++ backend/controllers/admin/index.js
@@ -12,6 +12,9 @@ const PERMISSION = ['admin']
 // model
 const AdminModel = require('../../models/admin')
 
+// controllers
+const QueryController = require('./query')
+
 router.get('/create', (req, res) => {
   res.sendFile(path.join(__dirname, '../../views/admin/', 'create.html'))
 })
@@ -40,15 +43,56 @@ router.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, '../../views/admin/', 'login.html'))
 })
 
-router.get('/logout', logout, (req, res) => res.redirect('/login'))
+router.get('/logout', logout, (req, res) => res.redirect('/admin/login'))
 
 // authen required
-router.use(['/'], authen({
+router.use(['/', '/edit', '/query'], authen({
   permission: PERMISSION,
   unauthorizedPath: '/admin/login'
 }))
+
 router.get('/', (req, res) => {
   res.send('admin jaaa')
 })
+
+router.get('/edit', async (req, res) => {
+  const id = req.session.passport.user._id
+  let user = await AdminModel.query.id(id)
+  if (!user) user = {}
+  res.render(path.join(__dirname, '../../views/admin/', 'edit'), { user: user })
+})
+
+router.post('/edit', (req, res) => {
+  const id = req.session.passport.user._id
+  const { username, password, email } = req.body
+  AdminModel.edit(id, {
+    username: username,
+    password: password,
+    email: email
+  })
+    .then(doc => res.send(doc))
+    .catch(err => res.send({
+      op: false,
+      err: String(err)
+    }))
+})
+
+router.get('/delete', (req, res) => {
+  const id = req.query.id
+  if (!id) {
+    return res.send({
+      op: false,
+      err: 'select id'
+    })
+  }
+  AdminModel.delete(id)
+    .then(doc => res.send(doc))
+    .catch(err => res.send({
+      op: false,
+      err: String(err)
+    }))
+})
+
+router.use('/query', QueryController)
 
 module.exports = router
---
+++ backend/controllers/admin/query.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+// models
+const AdminModel = require('../../models/admin/')
+
+router.get('/', (req, res) => {
+  const id = req.query.id || undefined
+  const limit = Number(req.query.limit) || undefined
+  const search = req.query.search || undefined
+
+  if (search) {
+    AdminModel.query.search(search, limit)
+      .then(doc => res.send(doc))
+  } else if (id) {
+    AdminModel.query.id(id)
+      .then(doc => res.send(doc))
+  } else {
+    AdminModel.query.all(limit)
+      .then(doc => {
+        res.send(doc)
+      })
+  }
+})
+
+module.exports = router

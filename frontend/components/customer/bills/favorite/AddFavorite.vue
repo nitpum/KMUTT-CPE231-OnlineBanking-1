915b3448aea42c4f3428fbcf2ed31f29e46ab333
topty <topty@live.com>
2019-05-28 21:30:05 +0700

    + bill lists

--- frontend/components/customer/bills/favorite/AddFavorite.vue
+++ frontend/components/customer/bills/favorite/AddFavorite.vue
@@ -1,35 +1,27 @@
 <template>
-  <v-dialog v-model="model" width="500px">
-    <v-btn slot="activator" color="primary">
+  <div>
+    <v-btn color="primary" @click="listsDialog = true">
       Add
     </v-btn>
-    <v-card>
-      <v-card-title class="headline" primary-title>
-        Title
-      </v-card-title>
-      <v-card-text>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
-        fuga tempora, perferendis rem libero error consectetur earum animi sed
-        id. Tenetur neque nemo tempora maxime molestias veniam obcaecati
-        inventore mollitia.
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn flat>
-          Close
-        </v-btn>
-        <v-btn flat color="primary">
-          Save
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+
+    <lists-dialog v-model="listsDialog" @select="select" />
+  </div>
 </template>
 
 <script>
+import ListsDialog from './../ListsDialog'
+
 export default {
+  components: {
+    ListsDialog
+  },
   data: () => ({
-    model: false
-  })
+    listsDialog: false
+  }),
+  methods: {
+    select(ref1) {
+      /* add bill to favorite here */
+    }
+  }
 }
 </script>

9fdb59d75456c4b8211f430e4a9e8f67ebbb5231
topty <topty@live.com>
2019-05-28 22:23:59 +0700

    + about 555555

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,9 +1,42 @@
 <template>
-  <v-container>
+  <v-container grid-list-lg>
     <v-card>
       <v-card-text>
-        about page
+        <v-layout row wrap justify-center>
+          <v-flex v-for="member in members" :key="member" xs12 sm6 md5 lg4 xl3>
+            <center>
+              <v-img
+                :src="`https://github.com/${member}.png`"
+                class="profile"
+                max-width="200px"
+                aspect-ratio="1"
+              />
+              <h3 class="headline">
+                <a
+                  href="`https://github.com/${member}`"
+                  style="text-decoration: none"
+                >
+                  @{{ member }}
+                </a>
+              </h3>
+            </center>
+          </v-flex>
+        </v-layout>
       </v-card-text>
     </v-card>
   </v-container>
 </template>
+
+<style>
+.profile {
+  border-radius: 3px !important;
+}
+</style>
+
+<script>
+export default {
+  data: () => ({
+    members: ['GrimxMaster', 'nitpum', 'topty', 'yee2542', 'yes2023']
+  })
+}
+</script>

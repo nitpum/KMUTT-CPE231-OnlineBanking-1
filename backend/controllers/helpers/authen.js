48b65283e5240af34b8c952c58bc65da36af5cb7
topty <topty@live.com>
2019-05-28 15:57:50 +0700

    + bypass auth path

--- backend/controllers/helpers/authen.js
+++ backend/controllers/helpers/authen.js
@@ -3,9 +3,11 @@
  * @param  {Array} options.permission - permission
  * @param  {String} options.unauthorizedPath - redirect after fail login
  * @param  {String} options.authorizedPath - redirect after success login
+ * @param  {Array} options.bypassPath - bypass authorization
  */
 module.exports = (options) => {
   return (req, res, next) => {
+    if ((options.bypassPath || []).some(path => path === req.originalUrl)) return next()
     if (!req.session) return res.sendStatus(401)
     if (!req.session.passport) return res.sendStatus(401)
     const user = req.session.passport.user

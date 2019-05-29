ce1b875419c3f221137d6f42df27f00fc2b19d5d
topty <topty@live.com>
2019-05-30 04:53:46 +0700

    fix undefinded router

--- frontend/pages/admin/system.vue
+++ frontend/pages/admin/system.vue
@@ -2,7 +2,7 @@
   <v-container grid-list-xl>
     <v-layout row wrap>
       <v-flex v-for="(card, i) in cards" :key="'ov-card-' + i" xs12 sm6>
-        <overview-card :title="card.title" :color="card.color" :to="to">
+        <overview-card :title="card.title" :color="card.color" :to="card.to">
           <span class="display-3">
             {{ card.value.split('/')[0] }}
           </span>

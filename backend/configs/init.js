13371e7117ae832d8d2d84991a9a00c803cb05de
GrimXmaster <athicha.aern@mail.kmutt.ac.th>
2019-05-30 03:35:13 +0700

    move init to config

--- backend/configs/init.js
+++ backend/configs/init.js
@@ -28,14 +28,17 @@ const account = [
 const branch = [
     {
      model :'branch',
+     schema:{
      name: 'Pracha-Utit',
      address: '555 Pracha-Utit road,thungKru,bangkok',
      zipcode: '10140',
      balance: 1,
+     }
     }
 ]
 const card =[{
     model:'card',
+    schema:{
     accountId: accountRef,
     holder: 'Tony Jaa',
     expired: 2022-02-09,
@@ -43,11 +46,12 @@ const card =[{
     pin: '123456',
     paymentDate:2019-05-30,
     isOnline: true
-
+    }
 }]
 
 const cheque=[{
     model: 'cheque',
+    schema:{
     chequeId: '900800700600500',
     byOrganizationId: organizationRef,
     accountId: accRef,
@@ -59,4 +63,32 @@ const cheque=[{
     drawee: 'Loki Adinson',
 
     drawDate: 2019-05-28
+    }
+}]
+const customer =[{
+    model:'customer',
+    schema:{
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    name: {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true }
+    },
+    email: { type: String, required: true },
+    zipcode: { type: String, maxlength: 5 },
+    address: String,
+    birthDate: Date,
+    gender: { type: String, enum: ['M', 'F', 'U'], required: true },
+    citizenId: { type: String, maxlength: 13, required: true },
+    phone: { type: String, maxlength: 13, required: true },
+    lastAccess: Date,
+    status: { type: String, enum: ['ACTIVE', 'LOCKED', 'TOKEN', 'RESETPASS', 'ETC'], default: 'ACTIVE' },
+    favorite: [favoriteRef],
+    notifications: [notifyRef],
+    account: [accRef],
+    balance: { type: Number, default: 0 },
+
+    dateCreate: { type: Date, default: Date.now }
+
+}
 }]

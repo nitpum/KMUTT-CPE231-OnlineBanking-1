d9f532be6a901cd111461f1cd7b3e5c978995291
yee2542 <yee2542gtl@gmail.com>
2019-05-29 22:46:55 +0700

    preparing trasaction function

--- backend/models/transaction/index.js
+++ backend/models/transaction/index.js
@@ -1,7 +1,23 @@
 const TransactionSchema = require('./schema')
 
+/**
+  * create transaction
+ * @param  {Object} data
+ * @param  {String} data.type - enum: ['TRANSFER', 'RECEIVE', 'WITHDRAW', 'ETC']
+ * @param  {Number} data.amount - amount of transaction
+ * @param  {String} data.surrogateName - surrogateName
+ * @param  {Object} data.accountId - accountId mongodb object id
+ * @param  {Object} data.chequeId - accountId mongodb object id
+ * @param  {Object} data.ref1 - ref1 mongodb object id
+ * @param  {Object} data.ref2 - ref2
+ * @returns {Object} - mongodb document
+ */
 const create = (data) => new Promise((resolve, reject) => {
-  resolve()
+  const doc = new TransactionSchema(data)
+  doc.save(err => {
+    if (err) reject(err)
+    resolve(doc)
+  })
 })
 
 module.exports = {

830d142c00f2a6cf7287db3c7ba3cb1eff6c4c07
Pakkapong Attawiboon <lukkik@windowslive.com>
2019-05-30 04:20:54 +0700

    add some analytic for admin

---
+++ frontend/pages/admin/overview/active-user.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>
+        <h3 class="headline mb-0">
+          Active User
+        </h3>
+      </v-card-title>
+      <v-divider />
+      <v-data-table :headers="headers" :items="items" class="elevation-1">
+        <template v-slot:items="{ item }">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.acessTime }}</td>
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+  data: () => ({
+    dialog: false,
+    dialogTitle: '',
+    headers: [
+      {
+        text: 'ID',
+        align: 'left',
+        value: 'id'
+      },
+      {
+        text: 'Name',
+        align: 'left',
+        value: 'name'
+      },
+      {
+        text: 'Acess Time',
+        align: 'center',
+        value: 'acessTime'
+      }
+    ],
+    items: [
+      {
+        id: '000',
+        name: 'Euei Naja',
+        acessTime: ''
+      },
+      {
+        id: '001',
+        name: 'Eiei Naja',
+        acessTime: ''
+      }
+    ],
+    setValue: null
+  })
+}
+</script>
---
+++ frontend/pages/admin/overview/today-trans.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>
+        <h3 class="headline mb-0">
+          Today Transaction
+        </h3>
+      </v-card-title>
+      <v-divider />
+      <v-data-table :headers="headers" :items="items" class="elevation-1">
+        <template v-slot:items="{ item }">
+          <td>{{ item.accId }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.totalTrans }}</td>
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+  data: () => ({
+    dialog: false,
+    dialogTitle: '',
+    headers: [
+      {
+        text: 'Account Id',
+        align: 'left',
+        value: 'accId'
+      },
+      {
+        text: 'Name',
+        align: 'left',
+        value: 'name'
+      },
+      {
+        text: 'Total Transaction',
+        align: 'center',
+        value: 'totalTrans'
+      }
+    ],
+    items: [
+      {
+        accId: '000',
+        name: 'Euei Naja',
+        totalTrans: 1515151515151
+      },
+      {
+        accId: '001',
+        name: 'Eiei Naja',
+        totalTrans: 1515151515151
+      }
+    ],
+    setValue: null
+  })
+}
+</script>

c6a16218744d44f8cb48f67565d55c843251a586
yee2542 <yee2542gtl@gmail.com>
2019-05-30 05:21:04 +0700

    add valid cheque

--- backend/models/cheque/transaction.js
+++ backend/models/cheque/transaction.js
@@ -4,13 +4,30 @@ const chequeModel = require('./schema')
  * @param  {Object} id - cheque id mongodb object
  * @returns {Object} - mongodb result object
  */
-const cashUp = (id) => new Promise((resolve, reject) => {
-  chequeModel.findByIdAndUpdate(id, { status: 'PAY' }, (err, doc) => {
-    if (err) reject(err)
+const cashUp = (id) => new Promise(async (resolve, reject) => {
+  try {
+    const doc = await chequeModel.findOne({ _id: id })
+    if (!doc) return reject(new Error('not found cheque'))
+    chequeModel.findByIdAndUpdate(id, { status: 'PAY' }, (err, doc) => {
+      if (err) reject(err)
+      resolve(doc)
+    })
+  } catch (err) {
+    reject(err)
+  }
+})
+
+const valid = (id) => new Promise(async (resolve, reject) => {
+  try {
+    const doc = await chequeModel.findOne({ _id: id })
+    if (!doc) return reject(new Error('not found cheque'))
     resolve(doc)
-  })
+  } catch (err) {
+    reject(err)
+  }
 })
 
 module.exports = {
-  cashUp: cashUp
+  cashUp: cashUp,
+  valid: valid
 }

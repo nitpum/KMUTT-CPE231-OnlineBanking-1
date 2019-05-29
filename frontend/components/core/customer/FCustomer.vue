5b57f69ae53e95cba010248ecaa90c1e72a41ce3
topty <topty@live.com>
2019-05-29 16:55:26 +0700

    + select customer

---
+++ frontend/components/core/customer/FCustomer.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="customerId"
+      label="Customer ID"
+      append-outer-icon="search"
+      @click:append-outer="select = () => selectFn"
+    />
+    <select-customer v-model="select" />
+  </div>
+</template>
+
+<script>
+import SelectCustomer from './SelectCustomer'
+
+export default {
+  components: {
+    SelectCustomer
+  },
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data: () => ({
+    select: null
+  }),
+  computed: {
+    customerId: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
+    }
+  },
+  methods: {
+    selectFn(val) {
+      this.customerId = val
+      this.select = null
+    }
+  }
+}
+</script>
---
+++ frontend/components/core/customer/SelectCustomer.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-dialog v-model="dialog" width="400">
+    <v-card>
+      <v-card-title class="headline">
+        Customer
+      </v-card-title>
+      <v-card-text>
+        <v-text-field
+          label="Search"
+          prepend-inner-icon="search"
+          solo
+          clearable
+        />
+        <v-divider />
+        <v-list two-line>
+          <v-list-tile
+            v-for="(customer, i) in customers"
+            :key="i"
+            @click="select(customer)"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ customer.name }}
+              </v-list-tile-title>
+              <v-list-tile-sub-title>
+                {{ customer.id }}
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn flat @click="dialog = false">
+          close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Function,
+      default: null
+    }
+  },
+  data: () => ({
+    customers: [
+      {
+        name: 'Korbboon Thuswongsa',
+        id: '123124'
+      }
+    ]
+  }),
+  computed: {
+    dialog: {
+      get() {
+        return !!this.value
+      },
+      set() {
+        this.$emit('input', null)
+      }
+    }
+  },
+  methods: {
+    select(customer) {
+      this.value()(customer.id)
+    }
+  }
+}
+</script>

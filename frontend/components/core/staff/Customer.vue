6c01dbf5b1ecd3c92fba7ced3bfb6b495e8727ff
nitpum <nitpum@outlook.com>
2019-05-30 06:29:10 +0700

    Customer editable

--- frontend/components/core/staff/Customer.vue
+++ frontend/components/core/staff/Customer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <v-card-title primary-title class="mb-0 pb-0">
-      <h3 class="headline mb-0">{{ name }}</h3>
+      <h3 class="headline mb-0">{{ name.firstName + ' ' + name.lastName }}</h3>
     </v-card-title>
     <v-card-text class="pt-0 pb-1 grey--text">
       {{ citizenId }}

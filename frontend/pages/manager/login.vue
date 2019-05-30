01e47bf14325253454f24acd86d8d4e4ec1afd01
topty <topty@live.com>
2019-05-30 08:09:52 +0700

    + color theme staff login

--- frontend/pages/manager/login.vue
+++ frontend/pages/manager/login.vue
@@ -1,18 +1,25 @@
 <template>
   <v-layout row justify-center align-center fill-height>
     <v-flex xs12 sm6 md4 lg3 xl2>
+      <h3 class="headline text-xs-center">
+        Manager
+      </h3>
       <login strategy="manager" />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import theme from '@/layouts/themes/manager'
 import Login from '@/components/Login'
 
 export default {
   layout: 'centered',
   components: {
     Login
+  },
+  created() {
+    this.$vuetify.theme = theme
   }
 }
 </script>

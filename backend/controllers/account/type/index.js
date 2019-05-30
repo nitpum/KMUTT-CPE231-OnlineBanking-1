0bdba6f3d520ffeae3373d6fe5f587c5f097f38b
nitpum <nitpum@outlook.com>
2019-05-30 07:50:28 +0700

    Account type now can edit and remove

--- backend/controllers/account/type/index.js
+++ backend/controllers/account/type/index.js
@@ -27,6 +27,27 @@ router.post('/create', (req, res) => {
     }))
 })
 
+router.post('/edit', (req, res) => {
+  const id = req.body.id
+  const data = req.body.data
+  AccountModel.type.edit(id, data)
+    .then(doc => res.send(doc))
+    .catch(err => res.status(400).send({
+      op: false,
+      err: String(err)
+    }))
+})
+
+router.delete('/', (req, res) => {
+  const id = req.body.id
+  AccountModel.type.delete(id)
+    .then(doc => res.send(doc))
+    .catch(err => res.status(400).send({
+      op: false,
+      err: String(err)
+    }))
+})
+
 router.use('/query', QueryControllers)
 
 module.exports = router

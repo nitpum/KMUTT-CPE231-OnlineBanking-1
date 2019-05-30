4d4b3349988afc7700ecf1e1c70434cca564b94c
nitpum <nitpum@outlook.com>
2019-05-30 10:45:41 +0700

    Add deposit and withdrawal

--- frontend/pages/staff/transactions/deposit.vue
+++ frontend/pages/staff/transactions/deposit.vue
@@ -10,6 +10,7 @@
             <v-card-text>
               <select-account v-model="account" />
               <v-text-field
+                v-model="amount"
                 label="Amount"
                 placeholder="0.00"
                 type="number"
@@ -18,24 +19,63 @@
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn flat>Close</v-btn>
-              <v-btn color="primary" flat>Submit</v-btn>
+              <v-btn color="primary" flat :disabled="disabled" @click="submit"
+                >Submit</v-btn
+              >
             </v-card-actions>
           </v-card>
         </v-flex>
       </v-layout>
+      <confirm-dialog
+        v-model="confirmDialog"
+        title="Deposit Confirm"
+        @onYes="create"
+      />
     </v-container>
   </div>
 </template>
 
 <script>
 import SelectAccount from '@/components/core/account/Select'
+import ConfirmDialog from '@/components/core/ConfirmDialog'
 
 export default {
   layout: 'staff',
-  components: { SelectAccount },
+  components: { SelectAccount, ConfirmDialog },
   data: () => ({
+    confirmDialog: false,
     account: null,
-    dialog: false
-  })
+    dialog: false,
+    amount: null,
+    message: ''
+  }),
+  computed: {
+    disabled() {
+      return !this.amount || !this.account
+    }
+  },
+  methods: {
+    submit() {
+      this.confirmDialog = true
+    },
+    create() {
+      this.message = `${this.amount} to ${this.account.accountId}`
+      this.$axios
+        .post('/transaction/staff/deposit', {
+          staffId: this.$store.state.user._id,
+          accountId: this.account._id,
+          amount: this.amount
+        })
+        .then(res => {
+          this.model = false
+          this.$emit('onSubmit').$store.dispatch('snackbars/success', 'Success')
+          this.account = {}
+          this.amount = null
+        })
+        .catch(e => {
+          this.$store.dispatch('snackbars/show', e.message)
+        })
+    }
+  }
 }
 </script>

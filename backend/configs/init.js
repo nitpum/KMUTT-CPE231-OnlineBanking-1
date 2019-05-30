25c39c2df2f599b61261b12588712feb17438bc0
yee2542 <yee2542gtl@gmail.com>
2019-05-30 07:35:09 +0700

    add bank init config

---
+++ backend/configs/init.js
@@ -0,0 +1,21 @@
+module.exports = {
+  organization: {
+    _id: '1',
+    name: 'KorbboonBank',
+    type: 'BANK',
+    bankSwift: 'KBTH',
+    bankAccount: '999999999999999'
+  },
+  serviceReferenceType: {
+    _id: '2',
+    name: 'Korbboon Bank OP',
+    detail: 'Korbboon Bank Operation'
+  },
+  serviceReference: {
+    organizationId: '1',
+    detail: 'Korbboon Bank operation system',
+    fee: 0,
+    typeId: '2'
+  },
+  
+}
